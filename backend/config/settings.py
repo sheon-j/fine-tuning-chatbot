5f7697ec052037b352e280dc43c383c3d6e9aed6
sheon-j <jsh7704@naver.com>
2023-12-13 23:15:14 +0900

    ADD backend settings

---
+++ backend/config/settings.py
@@ -0,0 +1,101 @@
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-c*l2!3^-2@-c%-#8#lws@!9$_1@0+rck9-xt$_+=@gnip&hz2y'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'rest_framework',
+    'corsheaders',
+
+    'fine_tuning_chatbot',
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleWare', # 맨 처음에 추가
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [ # 모든 출처 허용
+    'http://localhost:3000',
+]
+
+CORS_ALLOWED_ORIGINS = [ # 자격 증명 허용
+    'http://localhost:3000',
+]
+
+CORS_ORIGIN_ALLOW_ALL = True # 모든 도메인 요청 허용 (보안 위협)
+
+CORS_ALLOWED_CREDENTIALS = True
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Asia/Seoul'
+
+USE_I18N = True
+
+USE_TZ = False
+
+STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ backend/fine_tuning_chatbot/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FineTuningChatbotConfig(AppConfig):
+    name = 'fine_tuning_chatbot'
